e3406f0e7213e7491ff9a8429e6b9b93170bc308
mairin <jualex828@gmail.com>
2025-08-15 19:07:30 -0500

    SIMPLE

--- src/S2EJE5.java
+++ src/S2EJE5.java
@@ -1,48 +1,26 @@
 import java.util.Scanner;
+import java.util.Scanner;
 
+public class S2EJE5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-public class S2EJE5  {
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+        double peso;
+        double altura;
+        double imc;
 
-    // Variables
-    String nombre;     // Nombre del paciente
-    double peso;       // Peso en kilogramos
-    double estatura;   // Altura en metros
-    double imc;        // Índice de Masa Corporal
+        System.out.print("Peso en kg: ");
+        peso = sc.nextDouble();
 
-    // Entrada de datos
-    System.out.print("Ingrese el nombre del paciente: ");
-    nombre = sc.nextLine();
+        System.out.print("Altura en metros: ");
+        altura = sc.nextDouble();
 
-    System.out.print("Ingrese el peso en kg: ");
-    peso = sc.nextDouble();
+        imc = peso / (altura * altura);
 
-    System.out.print("Ingrese la estatura en metros (ej: 1.75): ");
-    estatura = sc.nextDouble();
+        System.out.println("Tu IMC es: " + imc);
 
-    // Cálculo del IMC
-    imc = peso / Math.pow(estatura, 2);
-
-    // Salida de datos
-    System.out.println("\n--- Resultado IMC ---");
-    System.out.println("Paciente: " + nombre);
-    System.out.println("Peso: " + peso + " kg");
-    System.out.println("Estatura: " + estatura + " m");
-    System.out.println("IMC: " + String.format("%.2f", imc));
-
-    // Clasificación del IMC (opcional)
-    if (imc < 18.5) {
-        System.out.println("Clasificación: Bajo peso");
-    } else if (imc < 25) {
-        System.out.println("Clasificación: Peso normal");
-    } else if (imc < 30) {
-        System.out.println("Clasificación: Sobrepeso");
-    } else {
-        System.out.println("Clasificación: Obesidad");
+        sc.close();
     }
+}
 
-    sc.close();
-}
-}
 
